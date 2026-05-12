139c445d326812d7887f0ac61a5b51d96fd2731e
r2220 <r2220617749@163.com>
2026-05-12 16:19:41 +0800

    后端代码

---
+++ src/main/java/com/example/test1/entity/PostLike.java
@@ -0,0 +1,16 @@
+package com.example.test1.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("post_likes")
+public class PostLike {
+    private Integer id;
+    private Integer userId;
+    private Integer postId;
+}
